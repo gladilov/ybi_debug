c643b03d8aa53511b06e3243aa6abbb1857a24cd
Alex Gl <a.gladilov@yandex.ru>
2016-05-06 16:40:32 +0300

    Change html/js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,20 @@
 
     var deviceReadyDeferred = $.Deferred();
     var jqmReadyDeferred = $.Deferred();
-    document.addEventListener("deviceReady", deviceReady, false);
+    
+    // are we running in native app or in a browser?
+    window.isphone = false;
+    if (document.URL.indexOf('http://') === -1 && document.URL.indexOf('https://') === -1) {
+      window.isphone = true;
+    }
+
+    if (window.isphone) {
+      document.addEventListener('deviceready', deviceReady, false);
+    } else {
+      deviceReady();
+    }
+    
+    //document.addEventListener("deviceReady", deviceReady, false);
 
     function deviceReady() {
       deviceReadyDeferred.resolve();
@@ -19,64 +32,85 @@
 
     function doWhenBothFrameworksLoaded() {
       // StatusBar
-      StatusBar.overlaysWebView( false );
-      StatusBar.backgroundColorByName("gray");
-      
+      if (window.isphone) {
+        StatusBar.overlaysWebView( false );
+        StatusBar.backgroundColorByName("gray");
+      }
       
       // Logic
       
       // TEST NOTIFICATION
       $('#test-notification').on('click', function(){
         alert('Test notification');
+        
         // Notification
-        navigator.notification.alert(
-          'Test',
-          null,
-          'Test notification',
-          'Закрыть'
-        );
+        if (window.isphone) {
+          navigator.notification.alert(
+            'Test',
+            null,
+            'Test notification',
+            'Закрыть'
+          );
+        }
       });
       
       // TEST AJAX
       $('#test-ajax').on('click', function(){
         alert('Test ajax start');
         
+        // post
+        $.post("http://y-b-i.com/api/debug.php", {'test': 'post_test_ok'}, function (responseData) {
+          data = $.parseJSON(responseData);
+          alert('post');
+          alert(data.data.test);
+        });
+        
+        // get
         $.ajax({
           type: 'GET',
           dataType: 'jsonp',
           jsonpCallback: 'userCreate',
-          url: 'http://y-b-i.com/api/user.php',
+          url: 'http://y-b-i.com/api/debug.php',
           data: {'method': 'POST', 'data': {'name': 'test', 'pass': '123'}},
           cache: false,
           async: true,
           crossDomain: true,
         })
         .done(function(data, textStatus, jqXHR){
+          alert('get');
           alert("success");
           console.log(data);
           
           // Notification
-          navigator.notification.alert(
-            data.status,
-            null,
-            'Test ajax jsonp - saccess',
-            'Закрыть'
-          );
+          if (window.isphone) {
+            navigator.notification.alert(
+              data.status,
+              null,
+              'Test ajax jsonp - saccess',
+              'Закрыть'
+            );
+          }
+          
+          alert('Test ajax stop');
         })
         .fail(function(jqXHR, textStatus, errorThrown){
           alert("error");
           console.log(data);
           
           // Notification
-          navigator.notification.alert(
-            jqXHR.responseText,
-            null,
-            'Test ajax jsonp - error',
-            'Закрыть'
-          );
+          if (window.isphone) {
+            navigator.notification.alert(
+              jqXHR.responseText,
+              null,
+              'Test ajax jsonp - error',
+              'Закрыть'
+            );
+          }
+          
+          alert('Test ajax stop');
         });
         
-        alert('Test ajax stop');
+        
       });
 
     }
