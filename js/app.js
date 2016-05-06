b10e2ea9bf3d269d9eef06c3617d3fa41cfe90e6
Alex Gl <a.gladilov@yandex.ru>
2016-05-06 16:19:30 +0300

    Change html/js

--- js/app.js
+++ js/app.js
@@ -9,33 +9,74 @@
     }
 
     $(document).on('mobileinit', function() {
-    //$(document).ready(function() {
+      $.support.cors = true;
+      $.mobile.allowCrossDomainPages = true;
+      
       jqmReadyDeferred.resolve();
-      console.log('mobileinit');
     });
 
     $.when(deviceReadyDeferred, jqmReadyDeferred).then(doWhenBothFrameworksLoaded);
-    //$.when(jqmReadyDeferred).then(doWhenBothFrameworksLoaded);
 
     function doWhenBothFrameworksLoaded() {
       // StatusBar
-      //StatusBar.overlaysWebView( false );
-      //StatusBar.backgroundColorByName("gray");
+      StatusBar.overlaysWebView( false );
+      StatusBar.backgroundColorByName("gray");
       
-      // Notification
-      /*navigator.notification.alert(
-        'Test',
-        null,
-        'Test notification',
-        'Закрыть'
-      );*/
       
       // Logic
+      
+      // TEST NOTIFICATION
       $('#test-notification').on('click', function(){
         alert('Test notification');
+        // Notification
+        navigator.notification.alert(
+          'Test',
+          null,
+          'Test notification',
+          'Закрыть'
+        );
       });
+      
+      // TEST AJAX
       $('#test-ajax').on('click', function(){
-        alert('Test ajax');
+        alert('Test ajax start');
+        
+        $.ajax({
+          type: 'GET',
+          dataType: 'jsonp',
+          jsonpCallback: 'userCreate',
+          url: 'http://y-b-i.com/api/user.php',
+          data: {'method': 'POST', 'data': {'name': 'test', 'pass': '123'}},
+          cache: false,
+          async: true,
+          crossDomain: true,
+        })
+        .done(function(data, textStatus, jqXHR){
+          alert("success");
+          console.log(data);
+          
+          // Notification
+          navigator.notification.alert(
+            data.status,
+            null,
+            'Test ajax jsonp - saccess',
+            'Закрыть'
+          );
+        })
+        .fail(function(jqXHR, textStatus, errorThrown){
+          alert("error");
+          console.log(data);
+          
+          // Notification
+          navigator.notification.alert(
+            jqXHR.responseText,
+            null,
+            'Test ajax jsonp - error',
+            'Закрыть'
+          );
+        });
+        
+        alert('Test ajax stop');
       });
 
     }
