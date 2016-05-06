0e4a8200956947e5354da49dcd548e3e3e6e688c
Alex Gl <a.gladilov@yandex.ru>
2016-05-06 15:30:16 +0300

    Change html/js

--- js/app.js
+++ js/app.js
@@ -1,41 +1,65 @@
 (function($) {
     "use strict";
+    
+    
+    var deviceReadyDeferred = $.Deferred();
+    var jqmReadyDeferred = $.Deferred();
 
-    var tipPercent = 15.0;
+    document.addEventListener("deviceReady", deviceReady, false);
 
-    var calcTip = function() {
-        var billAmt = Number( $('#billAmount').val() );
-        var tipAmt =  billAmt * tipPercent/100 ;
-        var totalAmt = billAmt + tipAmt;
-        $('#tipAmount').text('$' + tipAmt.toFixed(2));
-        $('#totalAmount').text('$' + totalAmt.toFixed(2));
-    };
+    function deviceReady() {
+      deviceReadyDeferred.resolve();
+    }
 
-    var saveSettings = function() {
-        try {
-            var tipPct = parseFloat( $('#tipPercentage').val() );
-            localStorage.setItem('tipPercentage', tipPct);
-            tipPercent = tipPct;
-            window.history.back();
-        } catch (ex) {
-            alert('Tip percentage must be a decimal value');
-        }
-    };
-
-    $( document ).on( "ready", function(){
-        $('#calcTip').on('click', calcTip);
-        $('#saveSettings').on('click', saveSettings);
-        var tipPercentSetting = localStorage.getItem('tipPercentage');
-        if (tipPercentSetting) {
-            tipPercent = parseFloat(tipPercentSetting);
-        }
-        $('#tipPercentage').val(tipPercent);
+    $(document).on("mobileinit", function () {
+      jqmReadyDeferred.resolve();
     });
 
-    $( document ).on( "deviceready", function(){
-        StatusBar.overlaysWebView( false );
-        StatusBar.backgroundColorByName("gray");
-    });
+    $.when(deviceReadyDeferred, jqmReadyDeferred).then(doWhenBothFrameworksLoaded);
+
+    function doWhenBothFrameworksLoaded() {
+      // StatusBar
+      StatusBar.overlaysWebView( false );
+      StatusBar.backgroundColorByName("gray");
+      
+      // Notification
+      navigator.notification.alert(
+        'Test',
+        null,
+        'Test notification',
+        'Закрыть'
+      );
+      
+      // Vars
+      var tipPercent = 15.0;
+      var calcTip = function() {
+          var billAmt = Number( $('#billAmount').val() );
+          var tipAmt =  billAmt * tipPercent/100 ;
+          var totalAmt = billAmt + tipAmt;
+          $('#tipAmount').text('$' + tipAmt.toFixed(2));
+          $('#totalAmount').text('$' + totalAmt.toFixed(2));
+      };
+      var saveSettings = function() {
+          try {
+              var tipPct = parseFloat( $('#tipPercentage').val() );
+              localStorage.setItem('tipPercentage', tipPct);
+              tipPercent = tipPct;
+              window.history.back();
+          } catch (ex) {
+              alert('Tip percentage must be a decimal value');
+          }
+      };
+      
+      
+      // Logic
+      $('#calcTip').on('click', calcTip);
+      $('#saveSettings').on('click', saveSettings);
+      var tipPercentSetting = localStorage.getItem('tipPercentage');
+      if (tipPercentSetting) {
+          tipPercent = parseFloat(tipPercentSetting);
+      }
+      $('#tipPercentage').val(tipPercent);
+    }
 
 }
 )(jQuery);
