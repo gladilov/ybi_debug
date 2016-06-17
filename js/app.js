76e1dd219881c6ba2c008f5fbed8584e2f7856b5
Alex Gl <a.gladilov@yandex.ru>
2016-06-17 14:45:04 +0300

    Change html/js and config.xml

--- js/app.js
+++ js/app.js
@@ -19,8 +19,6 @@
 
     function deviceReady() {
       deviceReadyDeferred.resolve();
-      
-      if (window.isphone) StatusBar.show();
     }
 
     $(document).on('mobileinit', function() {
@@ -150,6 +148,46 @@
             'Exit', // title
             ['Ok, exit','Cancel'] // buttonLabels
           );
+        }
+        else alert("Function work only mobile device.");
+      });
+      
+      // TEST TOAST
+      $('#toast-short-top').on('click', function(){
+        if (window.isphone) { window.plugins.toast.showShortTop('Hello there!', function(a){console.log('toast success: ' + a)}, function(b){alert('toast error: ' + b)}); }
+        else alert("Function work only mobile device.");
+      });
+      $('#toast-long-center').on('click', function(){
+        if (window.isphone) { window.plugins.toast.show('Hello there!', 'long', 'center', function(a){console.log('toast success: ' + a)}, function(b){alert('toast error: ' + b)}); }
+        else alert("Function work only mobile device.");
+      });
+      $('#toast-long-bottom').on('click', function(){
+        if (window.isphone) { window.plugins.toast.showLongBottom('Hello there!', function(a){console.log('toast success: ' + a)}, function(b){alert('toast error: ' + b)}); }
+        else alert("Function work only mobile device.");
+      });
+      
+      // TEST SOCIALSHARING
+      $('#test-socialsharing').on('click', function(){
+        if (window.isphone) {
+          // this is the complete list of currently supported params you can pass to the plugin (all optional)
+          var options = {
+            message: 'share this', // not supported on some apps (Facebook, Instagram)
+            subject: 'the subject', // fi. for email
+            files: ['', ''], // an array of filenames either locally or remotely
+            url: 'http://www.y-b-i.com',
+            chooserTitle: 'Pick an app' // Android only, you can override the default share sheet title
+          }
+
+          var onSuccess = function(result) {
+            console.log("Share completed? " + result.completed); // On Android apps mostly return false even while it's true
+            console.log("Shared to app: " + result.app); // On Android result.app is currently empty. On iOS it's empty when sharing is cancelled (result.completed=false)
+          }
+
+          var onError = function(msg) {
+            console.log("Sharing failed with message: " + msg);
+          }
+
+          window.plugins.socialsharing.shareWithOptions(options, onSuccess, onError);
         }
         else alert("Function work only mobile device.");
       });
